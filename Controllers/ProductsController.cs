f963ae69cac74924c584c8204e11219ac5aa054b
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-02-14 11:58:32 +0500

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using newProject.models;
+using Npgsql;
+
+namespace newProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ProductsController : ControllerBase
+    {
+
+        static string connString = "Server=localhost;Port=5432;Database=vs;User Id=postgres;Password=oktava;";
+        [HttpGet]
+        public List<model> Get()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connString))
+            {
+                string query = $"select * from new;";
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+                var x = command.ExecuteReader();
+
+                List<model> list = new List<model>();
+
+
+                while (x.Read())
+                {
+                    list.Add(new model()
+                    {
+                        Id = (int)x[0],
+                        Mavzu = (string)x[1],
+                        age = (int)x[2]
+                    });
+
+                }
+
+                return list;
+                connection.Close();
+
+            }
+        }
+
+
+    }
+    
+}
---
+++ models/model.cs
@@ -0,0 +1,9 @@
+﻿namespace newProject.models
+{
+    public class model
+    {
+        public int Id { get; set; }
+        public string Mavzu { get; set; }
+        public int age { get; set; }
+    }
+}
